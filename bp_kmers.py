614ab29451fdaeb145b6f181433d7c56b97085d9
shah.val <shah.val@husky.neu.edu>
2019-09-15 17:07:08 -0400

    last commit with the k-mers fasta file

--- bp_kmers.py
+++ bp_kmers.py
@@ -4,28 +4,34 @@ from Bio import SeqIO
 import re
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna
-def occurs_once(a,item):
-    return a.count(item)==1
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+from Bio.Alphabet import IUPAC
+
 for record in SeqIO.parse("/scratch/Drosophila/dmel-all-chromosome-r6.17.fasta","fasta"):
     sequence = str(record.seq)
-    #print(sequence)
-    #mysequence = Seq(sequence, generic_dna)
-    kmer_list = []
-    for i in range(len(sequence)):
-        seq = sequence[i:i+6]
-        if (len(seq)) == 6: 
-            kmer_list.append(str(seq))
-    kmer_dict = {}
-    for kmer in kmer_list:
-        if kmer in kmer_dict:
-            kmer_dict[kmer] +=1 
-        else:
-            kmer_dict[kmer]=0
-    uni = []
-    for kmer in kmer_dict.keys():
-        if kmer_dict[kmer]==0:
-             uni.append(kmer)
-print(len(kmer))
-        
-   
+kmer_list = []
+for i in range(len(sequence)):
+    seq = sequence[i:i+6]
+    if (len(seq)) == 6: 
+        kmer_list.append(str(seq))
+kmer_dict = {}
+for kmer in kmer_list:
+    if kmer in kmer_dict:
+        kmer_dict[kmer] +=1 
+    else:
+        kmer_dict[kmer]=0
+uni = []
+for kmer in kmer_dict.keys():
+    if kmer_dict[kmer]==0:
+         uni.append(kmer)
+print(len(uni))
+records = []
+uniq = {}
+for i in range(len(uni)):
+    uniq.update({i:uni[i]})
+for i in uniq.keys():
+    records.append(SeqRecord(Seq(uniq[i], generic_dna), str(i)))
+#my_seqs = SeqRecord(Seq(records, IUPAC.DNA), id = "randomsequence")
+SeqIO.write(records,"k-mers.fasta.gitignore", "fasta")
 
