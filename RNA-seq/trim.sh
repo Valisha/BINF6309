0c307f484833ab665b8f307b20235c9802590f48
shah.val <shah.val@husky.neu.edu>
2019-09-26 20:20:03 -0400

    check for trimAll.sh

--- RNA-seq/trim.sh
+++ RNA-seq/trim.sh
@@ -12,4 +12,4 @@ HEADCROP:0 \
 ILLUMINACLIP:/usr/local/programs/Trimmomatic-0.36/adapters/TruSeq3-PE.fa:2:30:10 \
 LEADING:20 TRAILING:20 SLIDINGWINDOW:4:30 MINLEN:36 \
 1>Aip02.trim.log 2>Aip02.trim.err
-#nice -n19 java -jar /usr/local/programs/Trimmomatic-0.36/trimmomatic-0.36.jar PE -threads -phred33 /scratch/AiptasiaMiSeq/fastq/Aip02.R1.fastq /scratch/AiptasiaMiSeq/fastq/Aip02.R2.fastq Aip02.R1.paired.fastq Aip02.R1.unpaired.fastq Aip02.R2.paired.fastq Aip02.R2.unpaired.fastq HEADCROP:0 ILLUMINACLIP:/usr/local/programs/Trimmomatic-0.36/adapters/TruSeq3-PE.fa:2:30:10 LEADING:20 TRAINLING:20 SLIDINGWINDOW:4:30 MINLEN:36 1>Aipo2.trim.log 2>Aip02.trim.err
+
